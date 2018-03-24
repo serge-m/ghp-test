ad2655a02e11b3ff46e4883ec2bd66dbb1ea0664
SergeM <sbmatyunin@gmail.com>
2018-03-24 13:34:25 +0100

    devcent video carousel using owl.carousel

--- js/main.js
+++ js/main.js
@@ -90,20 +90,22 @@
 		}
 	});
 
-    $('#videos-slider').owlCarousel({
-        items: 1,
-        merge: true,
-        loop: true,
-        margin: 10,
-        video: true,
-        lazyLoad: true,
-        center: true,
-        responsive: {
-            480: {
-                items: 2
+    $('.video-slider').owlCarousel({
+        items:1,
+        merge:false,
+        loop:true,
+        margin:10,
+        video:true,
+        lazyLoad:true,
+        center:false,
+        nav:true,
+		dots: false,
+        responsive:{
+            480:{
+                items:2
             },
-            600: {
-                items: 4
+            600:{
+                items:4
             }
         }
     });
